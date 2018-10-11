6d50ff31b3643641d70d237e3db5689794577f11
angelikasalvadora <angelika.salvadora@gmail.com>
2018-10-11 23:16:30 +0800

    module8

--- config.js
+++ config.js
@@ -12,10 +12,10 @@ const config = {
   },
   production: {
     db: {
-      database: process.env.d1oe598mlghr2k,
-      user: process.env.ovmoigrkqlygem,
-      password: process.env.d05178ad3051ccc4bce492f7b83dfc1e2a116aea7671cd1c55ddd3a32df68a89,
-      host: process.env.ec2-23-21-147-71.compute-1.amazonaws.com,
+      database: 'd1oe598mlghr2k',
+      user: 'ovmoigrkqlygem',
+      password: 'd05178ad3051ccc4bce492f7b83dfc1e2a116aea7671cd1c55ddd3a32df68a89',
+      host: 'ec2-23-21-147-71.compute-1.amazonaws.com',
       port: 5432,
       ssl: true
     },
